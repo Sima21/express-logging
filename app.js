3807ea560791afb50288379a85834e10a4d2a523
Sima21 <af.akhtari@yahoo.de>
2024-06-14 23:21:45 +0200

    Änderung in app.js

--- app.js
+++ app.js
@@ -5,10 +5,8 @@ const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
-// Verwende Morgan, um Anfragedetails zu loggen
 app.use(morgan('combined'));
 
-// Definiere die Routen
 app.get('/', (req, res) => {
   res.send('GET /');
 });
@@ -29,13 +27,12 @@ app.delete('/data', (req, res) => {
   res.send('DELETE /data');
 });
 
-// Zentrale Fehlerbehandlungs-Middleware
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
 
-// Starte den Server
 app.listen(port, () => {
   console.log(`Server läuft unter http://localhost:${port}`);
 });
